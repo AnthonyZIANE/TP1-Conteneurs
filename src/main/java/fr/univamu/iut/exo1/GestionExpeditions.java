deb13d70b125374605ea6af14be807670ff7cde6
z18011806 <anthony.ziane@etu.univ-amu.fr>
2019-11-21 17:30:23 +0100

    exo 1

--- src/main/java/fr/univamu/iut/exo1/GestionExpeditions.java
+++ src/main/java/fr/univamu/iut/exo1/GestionExpeditions.java
@@ -1,7 +1,18 @@
 package fr.univamu.iut.exo1;
 
+import fr.univamu.iut.Conteneur;
+
+import java.util.ArrayList;
+
 public class GestionExpeditions {
-    public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented !");
+    public static void main(String[] args)
+    {
+        Lecteur monLecteur = new Lecteur();
+        ArrayList<Conteneur> maListe = monLecteur.charger("Conteneurs.ser");
+        for (int i = 0 ; i < maListe.size() ; ++i)
+            System.out.println(maListe.get(i).toString());
+        //throw new RuntimeException("Not yet implemented !");
     }
 }
+
+
--- src/main/java/fr/univamu/iut/exo1/Lecteur.java
+++ src/main/java/fr/univamu/iut/exo1/Lecteur.java
@@ -2,11 +2,45 @@ package fr.univamu.iut.exo1;
 
 import fr.univamu.iut.Conteneur;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class Lecteur {
-    public static Collection<Conteneur> charger(String fichier) {
-        // À compléter
-        return null;
+    public static ArrayList<Conteneur> charger(String fichier)
+    {
+        ArrayList<Conteneur> maListe = new ArrayList<Conteneur>();
+        try{
+            File monFichier = new File(fichier);
+            ObjectInputStream monStream = new ObjectInputStream(new FileInputStream(monFichier));
+
+            try
+            {
+                while (true)
+                {
+                    Conteneur monConteneur = null;
+                    monConteneur = (Conteneur) monStream.readObject();
+                    if (monConteneur.getClass().equals(Conteneur.class))
+                    maListe.add(monConteneur);
+                }
+            }
+            catch (IOException ex)
+            {
+                System.out.println("Fin de fichier");
+            }
+        }
+        catch(IOException ex)
+        {
+            System.out.println("IOException is caught");
+        }
+        catch (ClassNotFoundException ex)
+        {
+            System.out.println("ClassNotFoundException" +
+                    " is caught");
+        }
+        return maListe;
     }
 }
